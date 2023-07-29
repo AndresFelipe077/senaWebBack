5716067b32200180e63bcd0b9d2a9e2f3335bff7
smith <jhonsenapro@gmail.com>
2023-07-29 10:14:56 -0500

    elmentos creados en criterios

---
+++ app/Models/criteriosEvaluacion.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class criteriosEvaluacion extends Model
+{
+    use HasFactory;
+    protected $table = 'criteriosEvaluacion';
+
+    protected $guarded = [];
+
+
+}
