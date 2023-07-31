e9ef1578a91c67b3db4a27c53564bcf5108c05c8
unknown <Formacin@POPDAFCCSGSP005.FormacionSena.RED>
2023-07-31 11:48:41 -0500

    cambios para bajar

--- app/Http/Controllers/ProyectoFormativoController.php
+++ app/Http/Controllers/ProyectoFormativoController.php
@@ -44,22 +44,19 @@ class ProyectoFormativoController extends Controller
         };
 
         //quitar pivots
-        // $newProyecto = $proyectoFormativo->map(function ($proyecto) {
-        //     $proyecto['fases'] = $proyecto['fases']->map(function ($proyectoF) {
-        //         $pivot = $proyectoF['pivot'];
-        //         unset($proyectoF['pivot']);
-        //         $proyectoF['fase_proyecto'] = $pivot;
-        //         return $proyectoF;
-        //     });
+        $newProyecto = $proyectoFormativo->map(function ($proyecto) {
+            $proyecto['fases'] = $proyecto['fases']->map(function ($proyectoF) {
+                $pivot = $proyectoF['pivot'];
+                unset($proyectoF['pivot']);
+                $proyectoF['fase_proyecto'] = $pivot;
+                return $proyectoF;
+            });
 
-        //     $Proyecto = asignacionCompetenciaProyecto::with('competencias', 'proyectosFormativos')
-        //     // ->where('company_id', $id)
-        //     ->get();
+            $Proyecto = asignacionCompetenciaProyecto::with('competencias', 'proyectosFormativos')
+            ->get();
 
-        // return response()->json($Proyecto);
-
-        //     // return $proyecto;
-        // });
+            return $proyecto;
+        });
         
         return response()->json($proyectoFormativo);
     }
