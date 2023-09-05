1de8d064179664320c2c9fa4d70669005cacc2f7
Andres Felipe <pizoluligoa@gmail.com>
2023-09-05 12:08:26 -0500

    Add new table about ActividadEvento

---
+++ app/Models/ActividadEvento.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ActividadEvento extends Model
+{
+    use HasFactory;
+
+    protected $table = "actividadEvento";
+
+    protected $guarded = [];
+
+    public function infraestructura(): BelongsTo
+    {
+        return $this->belongsTo(Infraestructura::class);
+    }
+
+    public function asignacion(): BelongsTo
+    {
+        return $this->belongsTo(AsignacionParticipante::class);
+    }
+
+}
---
+++ database/migrations/2023_09_05_165630_create_actividad_eventos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActividadEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividadEvento', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('idInfraestructura')->onDelete('cascade')->nullable();
+            $table->foreign('idInfraestructura')->references('id')->on('infraestructura')->onDelete('cascade');
+
+            $table->text('observacion');
+
+            $table->date('fechaInicial');
+            $table->date('fechaFinal')->nullable();
+
+            $table->unsignedInteger('idParticipante')->nullable();
+            $table->foreign('idParticipante')->references('id')->on('usuario')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividad_eventos');
+    }
+}
