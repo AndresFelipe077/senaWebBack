770a7524bc014a4206036d6b017f29c27f5922aa
Andres Felipe <pizoluligoa@gmail.com>
2023-09-20 10:20:34 -0500

    descomment seeder of proyectoFormativo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -99,7 +99,7 @@ class DatabaseSeeder extends Seeder
         $path = 'database/seeders/sql/tipo_oferta.sql';
         DB::unprepared(file_get_contents($path));
 
-        // proyectoFormativo::factory(1)->create();
+        proyectoFormativo::factory(1)->create();
 
         // Grupo::factory(1)->create();
 
